81b95d28066ca99286bc75c865dba46fef959a36
Marco BARNIG <mbarnig@pt.lu>
2025-09-05 20:34:04 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  trailingSlash: true
+  trailingSlash: true,
+  reactStrictMode: false,  // évite les effets déclenchés 2× en dev
+  experimental: {
+    // si tu as des warnings "workspace root" : force la racine ici
+    outputFileTracingRoot: __dirname
+  }
 };
 module.exports = nextConfig;
+
