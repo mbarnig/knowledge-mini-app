7e47d96a9fdbbff11fdb75d7db65152af73c90e0
Marco BARNIG <mbarnig@pt.lu>
2025-09-05 21:12:04 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -5,7 +5,14 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: false,
   // ✅ moved here (not in experimental)
-  outputFileTracingRoot: __dirname,
+  // outputFileTracingRoot: __dirname,
+  
+  // ✅ masque le badge “N” en dev
+  devIndicators: {
+    buildActivity: false,
+    // (optionnel) si tu veux juste le déplacer au lieu de le cacher :
+    // buildActivityPosition: 'bottom-right',
+  },
 };
 module.exports = nextConfig;
 
